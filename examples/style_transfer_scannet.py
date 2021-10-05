d105e751def1cc3a87ccd3fade04a3d016467480
Lukas Hoellein <lukas.hoellein@gmail.com>
2021-10-05 09:03:28 +0200

    simplify the datasets

--- examples/style_transfer_scannet.py
+++ examples/style_transfer_scannet.py
@@ -89,22 +89,22 @@ class Model(nn.Module):
         s, c, tv = self.vgg_loss(image, content_img, self.style_image, mask)
         loss = self.lambda_content * c + self.lambda_style * s + self.lambda_tv * tv
 
-        #print(f"C: {c}, S: {s}, TV: {tv}")
-        """
+        print(f"C: {c}, S: {s}, TV: {tv}")
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots(1, 3)
         ax[0].imshow(torchvision.transforms.ToPILImage()(content_img.squeeze()))
         ax[1].imshow(torchvision.transforms.ToPILImage()(image.squeeze()))
         ax[2].imshow(mask.cpu().numpy())
         plt.show()
-        """
 
         return loss, image
 
 
 def make_gif(filename, pattern='/tmp/_tmp_*.png', remove=True):
     with imageio.get_writer(filename, mode='I') as writer:
-        for filename in sorted(glob.glob(pattern)):
+        files = glob.glob(pattern)
+        files = sorted(files, key=lambda x: int(x.split(".")[0].split("_")[-1]))
+        for filename in files:
             writer.append_data(imageio.imread(filename))
             if remove:
                 os.remove(filename)
@@ -125,16 +125,17 @@ def get_key(args):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-io', '--mesh', type=str, default="/home/hoellein/datasets/scannet/train/scans/scene0673_00/scene0673_00_vh_clean_decimate_500000_uvs_blender.obj")
+    parser.add_argument('-dp', '--data_path', type=str, default="/home/hoellein/datasets/scannet/train/")
+    parser.add_argument('-if', '--image_folder', type=str, default="_additional")
     parser.add_argument('-s', '--scene', type=str, default="scene0673_00")
     parser.add_argument('-is', '--filename_style', type=str, default="/home/hoellein/datasets/styles/3style/14-2.jpg")
     parser.add_argument('-d', '--out_dir', type=str, default=os.path.join(data_dir))
     parser.add_argument('-vgg', '--vgg_model_path', type=str, default="/home/hoellein/models/vgg_conv.pth")
-    parser.add_argument('-lc', '--lambda_content', type=float, default=0)
+    parser.add_argument('-lc', '--lambda_content', type=float, default=1e2)
     parser.add_argument('-ls', '--lambda_style', type=float, default=1)
-    parser.add_argument('-ltv', '--lambda_tv', type=float, default=1e7)
-    parser.add_argument('-e', '--epochs', type=int, default=100)
-    parser.add_argument('-ln', '--log_nth', type=int, default=20)
+    parser.add_argument('-ltv', '--lambda_tv', type=float, default=1e8)
+    parser.add_argument('-e', '--epochs', type=int, default=10)
+    parser.add_argument('-ln', '--log_nth', type=int, default=1)
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.1)
     parser.add_argument('-mi', '--max_images', type=int, default=-1)
     parser.add_argument('-ir', '--index_repeat', type=int, default=1)
@@ -142,35 +143,47 @@ def main():
     parser.add_argument('-g', '--gpu', type=int, default=0)
     args = parser.parse_args()
 
+    # construct out path
     dir, key = get_key(args)
     Path(dir).mkdir(parents=True, exist_ok=True)
     out_prefix = os.path.join(dir, key)
-    d = ScanNet_Single_House_Dataset(root_path="/home/hoellein/datasets/scannet/train/images",
+
+    # build dataset
+    image_path = os.path.join(args.data_path, args.image_folder)
+    d = ScanNet_Single_House_Dataset(root_path=image_path,
                                      scene=args.scene,
                                      verbose=True,
                                      transform_rgb=torchvision.transforms.ToTensor(),
-                                     load_uvs=True,
                                      resize=True,
                                      resize_size=(args.size, args.size),
                                      max_images=1000,
                                      min_images=1)
 
-    model = Model(args.mesh, args.filename_style, args.vgg_model_path,
+    # construct model with mesh, style image and vgg
+    mesh_name = f'{args.scene}_vh_clean_decimate_500000_uvs_blender.obj'
+    mesh_path = os.path.join(args.data_path, 'scans', args.scene, mesh_name)
+    model = Model(mesh_path, args.filename_style, args.vgg_model_path,
                   lambda_content=args.lambda_content,
                   lambda_style=args.lambda_style,
                   lambda_tv=args.lambda_tv,
                   image_size=args.size)
     model.cuda()
 
-    # optimize
+    # optimize textured mesh
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
     for epoch in range(args.epochs):
+        # set up logging and saving
         items = tqdm(d)
         items.set_description(f"Epoch {epoch}")
         out_prefix_epoch = f'{out_prefix}_epoch_{epoch}'
+
+        # optimize one epoch
         for i, (rgb, extrinsics, intrinsics, mask) in enumerate(tqdm(d)):
+            # break epoch after this many images
             if -1 < args.max_images <= i:
                 break
+
+            # optimize each image this many times
             for _ in range(args.index_repeat):
                 optimizer.zero_grad()
                 loss, image = model(rgb.unsqueeze(0).cuda(), extrinsics, intrinsics, mask.cuda())
@@ -178,15 +191,15 @@ def main():
                 items.set_postfix({"loss": loss.cpu().detach().numpy().item()})
                 optimizer.step()
 
+            # save optimized image directly (not after the epoch is done, this is faster)
             if epoch % args.log_nth == 0 or args.log_nth == -1:
                 image = image.detach().cpu().numpy()[0].transpose((1, 2, 0))
-                imsave('/tmp/_tmp_%04d.png' % i, image)
-
                 mask3 = torch.stack([mask, mask, mask], dim=0)  # from (H x W) to (3 x H x W)
                 image_masked = np.where(mask3.detach().cpu().permute(1,2,0).numpy(), image, np.zeros_like(image))
                 imsave(f'{out_prefix_epoch}_{i}.png', image_masked)
+
+        # after each epoch, we collect the images and make a .gif out of them
         if epoch % args.log_nth == 0 or args.log_nth == -1:
-            make_gif(f"{out_prefix_epoch}.gif")
             make_gif(f"{out_prefix_epoch}.gif", f"{out_prefix_epoch}*.png", remove=False)
 
 
---
+++ examples/style_transfer_scannet_all.sh
@@ -0,0 +1,9 @@
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0027_00 -is /home/hoellein/datasets/styles/120styles/5.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0291_00 -is /home/hoellein/datasets/styles/120styles/20.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0293_00 -is /home/hoellein/datasets/styles/120styles/50.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0362_02 -is /home/hoellein/datasets/styles/120styles/1.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0401_00 -is /home/hoellein/datasets/styles/120styles/10.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0481_01 -is /home/hoellein/datasets/styles/120styles/0.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0568_02 -is /home/hoellein/datasets/styles/120styles/15.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0592_00 -is /home/hoellein/datasets/styles/120styles/3.jpg -d ~/results/baselines/NMR
+cd ~/git/neural_renderer; python examples/style_transfer_scannet.py -s scene0673_00 -is /home/hoellein/datasets/styles/3style/14-2.jpg -d ~/results/baselines/NMR
